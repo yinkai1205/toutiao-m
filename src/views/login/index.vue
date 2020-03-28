d530db9d42174b5d29a8c36a8b1dc465124f80b9
lipengzhou <lpzmail@163.com>
2020-03-28 12:55:44 +0800

    登录注册-实现基本登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,13 +7,14 @@
     <!-- 登录表单 -->
     <van-form @submit="onSubmit">
       <van-field
-        name="用户名"
+        v-model="user.mobile"
+        name="手机号"
         placeholder="请输入手机号"
       >
         <i slot="left-icon" class="toutiao toutiao-shouji"></i>
       </van-field>
       <van-field
-        type="password"
+        v-model="user.code"
         name="验证码"
         placeholder="请输入验证码"
       >
@@ -33,12 +34,18 @@
 </template>
 
 <script>
+import { login } from '@/api/user'
+
 export default {
   name: 'LoginIndex',
   components: {},
   props: {},
   data () {
     return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
     }
   },
   computed: {},
@@ -46,8 +53,25 @@ export default {
   created () {},
   mounted () {},
   methods: {
-    onSubmit (values) {
-      console.log('submit', values)
+    async onSubmit () {
+      // 1. 获取表单数据
+      const user = this.user
+
+      // TODO: 2. 表单验证
+
+      // 3. 提交表单请求登录
+      try {
+        const res = await login(user)
+        console.log('登录成功', res)
+      } catch (err) {
+        if (err.response.status === 400) {
+          console.log('手机号或验证码错误')
+        } else {
+          console.log('登录失败，请稍后重试', err)
+        }
+      }
+
+      // 4. 根据请求响应结果处理后续操作
     }
   }
 }
