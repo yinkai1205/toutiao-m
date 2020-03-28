6cef17bf963d90114bc33054a1e77e57f01fd92a
lipengzhou <lpzmail@163.com>
2020-03-28 11:55:20 +0800

    登录注册-准备-布局样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登录" />
+    <!-- /导航栏 -->
+
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        name="用户名"
+        placeholder="请输入手机号"
+      >
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field
+        type="password"
+        name="验证码"
+        placeholder="请输入验证码"
+      >
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button class="send-sms-btn" round size="small" type="default">发送验证码</van-button>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+    <!-- /登录表单 -->
+  </div>
 </template>
 
 <script>
@@ -8,14 +38,42 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit (values) {
+      console.log('submit', values)
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
