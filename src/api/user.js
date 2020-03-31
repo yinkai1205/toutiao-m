ae21c794e2928697ec3fe73de217599f46755a83
lipengzhou <lpzmail@163.com>
2020-03-31 20:07:37 +0800

    个人中心-优化设置Token

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@
  * 用户相关请求模块
  */
 import request from '@/utils/request'
-import store from '@/store'
+// import store from '@/store'
 
 export const login = data => {
   return request({
@@ -29,12 +29,12 @@ export const sendSms = mobile => {
 export const getUserInfo = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
+    url: '/app/v1_0/user'
     // 发送请求头数据
-    headers: {
-      // 注意：该接口需要授权才能访问
-      //       token的数据格式：Bearer token数据，注意 Bearer 后面有个空格
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    // headers: {
+    //   // 注意：该接口需要授权才能访问
+    //   //       token的数据格式：Bearer token数据，注意 Bearer 后面有个空格
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
   })
 }
