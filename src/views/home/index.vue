f9a7adf4bc0774754efc38775734e0d067e2ccef
lipengzhou <lpzmail@163.com>
2020-04-02 21:57:01 +0800

    首页-处理文章频道列表-样式调整（处理汉堡按钮）

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -15,9 +15,9 @@
 
     <!-- 频道列表 -->
     <!--
-      通过v-model绑定当前激活标签对应的索引值，默认情况下启用第一个标签
-      通过animated属性可以开启切换标签内容时的转场动画
-      通过swipeable属性可以开启滑动切换标签页
+      通过 v-model 绑定当前激活标签对应的索引值，默认情况下启用第一个标签
+      通过 animated 属性可以开启切换标签内容时的转场动画
+      通过 swipeable 属性可以开启滑动切换标签页
      -->
     <van-tabs class="channel-tabs" v-model="active" animated swipeable>
       <van-tab title="标签 1">内容 1</van-tab>
@@ -26,6 +26,10 @@
       <van-tab title="标签 4">内容 4</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="toutiao toutiao-gengduo"></i>
+      </div>
     </van-tabs>
     <!-- /频道列表 -->
   </div>
@@ -94,6 +98,36 @@ export default {
       width: 31px !important;
       height: 6px;
       background-color: #3296fa;
+    }
+
+    .placeholder {
+      flex-shrink: 0;
+      width: 66px;
+      height: 82px;
+    }
+
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 66px;
+      height: 82px;
+      background-color: #fff;
+      opacity: 0.902;
+      i.toutiao {
+        font-size: 33px;
+      }
+      &:before {
+        content: "";
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 100%;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
     }
   }
 }
