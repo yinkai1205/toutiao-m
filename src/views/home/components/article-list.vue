7f10c59a1423f596d4c3de8261cd6dc4c4fba368
lipengzhou <lpzmail@163.com>
2020-04-03 21:54:52 +0800

    首页-展示文章列表-下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -20,20 +20,27 @@
         + 在每次请求完毕后，需要手动将 loading 设置为 false，表示本次加载结束
         + 所有数据加载结束，finished 为 true，此时不会触发 load 事件
      -->
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      :error.sync="error"
-      error-text="请求失败，点击重新加载"
-      @load="onLoad"
+    <van-pull-refresh
+      v-model="isreFreshLoading"
+      :success-text="refreshSuccessText"
+      :success-duration="1500"
+      @refresh="onRefresh"
     >
-      <van-cell
-        v-for="(article, index) in list"
-        :key="index"
-        :title="article.title"
-      />
-    </van-list>
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        :error.sync="error"
+        error-text="请求失败，点击重新加载"
+        @load="onLoad"
+      >
+        <van-cell
+          v-for="(article, index) in list"
+          :key="index"
+          :title="article.title"
+        />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -55,7 +62,9 @@ export default {
       loading: false, // 控制加载中 loading 状态
       finished: false, // 控制数据加载结束的状态
       timestamp: null, // 请求获取下一页数据的时间戳
-      error: false // 控制列表加载失败的提示状态
+      error: false, // 控制列表加载失败的提示状态
+      isreFreshLoading: false, // 控制下拉刷新的 loading 状态
+      refreshSuccessText: '刷新成功' // 下拉刷新成功提示文本
     }
   },
   computed: {},
@@ -104,31 +113,37 @@ export default {
         // 请求失败了，loading 也需要关闭
         this.loading = false
       }
+    },
+
+    // 当下拉刷新的时候会触发调用该函数
+    async onRefresh () {
+      try {
+        // 1. 请求获取数据
+        const { data } = await getArticles({
+          channel_id: this.channel.id, // 频道ID
+          timestamp: Date.now(), // 下拉刷线，每次请求获取最新数据，所以传递当前最新时间戳
+          with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+        })
+
+        // 模拟随机失败的情况
+        // if (Math.random() > 0.2) {
+        //   JSON.parse('dsnajndjsa')
+        // }
+
+        // 2. 将数据追加到列表的顶部
+        const { results } = data.data
+        this.list.unshift(...results)
+
+        // 3. 关闭下拉刷新的 loading 状态
+        this.isreFreshLoading = false
+
+        // 更新下拉刷新成功提示的文本
+        this.refreshSuccessText = `刷新成功，更新了${results.length}条数据`
+      } catch (err) {
+        this.refreshSuccessText = '刷新失败'
+        this.isreFreshLoading = false
+      }
     }
-    // 初始化或滚动到底部的时候会触发调用 onLoad
-    // onLoad () {
-    //   // 1. 请求获取数据
-    //   // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-    //   setTimeout(() => {
-    //     // 2. 把请求结果数据放到 list 数组中
-    //     for (let i = 0; i < 10; i++) {
-    //       // 0 + 1 = 1
-    //       // 1 + 1 = 2
-    //       // 2 + 1 = 3
-    //       this.list.push(this.list.length + 1)
-    //     }
-
-    //     // 3. 本次数据加载结束之后要把加载状态设置为结束
-    //     //     loading 关闭以后才能触发下一次的加载更多
-    //     this.loading = false
-
-    //     // 4. 判断数据是否全部加载完成
-    //     if (this.list.length >= 40) {
-    //       // 如果没有数据了，把 finished 设置为 true，之后不再触发加载更多
-    //       this.finished = true
-    //     }
-    //   }, 1000)
-    // }
   }
 }
 </script>
