e8ba9d867c9ba90bc3a52bec67606e39ba9f9b38
lipengzhou <lpzmail@163.com>
2020-04-04 12:57:52 +0800

    首页-展示文章列表-固定头部

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
     <!-- 导航栏 -->
-    <van-nav-bar class="page-nav-bar">
+    <van-nav-bar class="page-nav-bar" fixed>
       <van-button
         class="search-btn"
         slot="title"
@@ -75,6 +75,7 @@ export default {
 
 <style scoped lang="less">
 .home-container {
+  padding-top: 174px;
   padding-bottom: 100px;
   .van-nav-bar__title {
     max-width: unset;
@@ -99,6 +100,11 @@ export default {
     }
 
     .van-tabs__wrap {
+      position: fixed;
+      top: 92px;
+      z-index: 1;
+      left: 0;
+      right: 0;
       height: 82px;
     }
 
