c53c1866c4b76fd7fca646434d04c86d0459bce9
lipengzhou <lpzmail@163.com>
2020-04-06 23:26:46 +0800

    频道编辑-准备-页面布局

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="channel-edit">频道编辑</div>
+  <div class="channel-edit">
+    <van-cell :border="false">
+      <div slot="title">我的频道</div>
+      <van-button
+        type="danger"
+        plain
+        round
+        size="mini"
+      >编辑</van-button>
+    </van-cell>
+    <van-grid :gutter="10">
+      <van-grid-item
+        v-for="value in 8"
+        :key="value"
+        text="文字"
+      />
+    </van-grid>
+
+    <!-- 频道推荐 -->
+    <van-cell :border="false">
+      <div slot="title">频道推荐</div>
+    </van-cell>
+    <van-grid :gutter="10">
+      <van-grid-item
+        v-for="value in 8"
+        :key="value"
+        text="文字"
+      />
+    </van-grid>
+    <!-- /频道推荐 -->
+  </div>
 </template>
 
 <script>
@@ -18,4 +48,8 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.channel-edit {
+  padding: 85px 0;
+}
+</style>
