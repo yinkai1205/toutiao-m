ad5c838e562bf059dacb3c3cc7858ba17677edc5
lipengzhou <lpzmail@163.com>
2020-04-08 01:11:21 +0800

    文章搜索-联想建议-请求获取展示数据

---
+++ src/api/search.js
@@ -0,0 +1,14 @@
+/**
+ * 用户相关请求模块
+ */
+import request from '@/utils/request'
+
+export const getSearchSuggestions = q => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/suggestion',
+    params: {
+      q
+    }
+  })
+}
--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="search-suggestion">
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
+    <van-cell
+      icon="search"
+      :title="text"
+      v-for="(text, index) in suggestions"
+      :key="index"
+    ></van-cell>
   </div>
 </template>
 
 <script>
+import { getSearchSuggestions } from '@/api/search'
+
 export default {
   name: 'SearchSuggestion',
   components: {},
@@ -19,7 +22,9 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      suggestions: [] // 联想建议数据列表
+    }
   },
   computed: {},
   watch: {
@@ -27,14 +32,23 @@ export default {
       // 当 searchText 发生改变的时候就会调用 handler 函数
       // 注意：handler 函数名称是固定的
       handler (value) {
-        console.log(value)
+        this.loadSearchSuggestions(value)
       },
       immediate: true // 该回调将会在侦听开始之后被立即调用
     }
   },
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    async loadSearchSuggestions (q) {
+      try {
+        const { data } = await getSearchSuggestions(q)
+        this.suggestions = data.data.options
+      } catch (err) {
+        this.$toast('数据获取失败，请稍后重试')
+      }
+    }
+  }
 }
 </script>
 
