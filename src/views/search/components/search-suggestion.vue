5ca16f72e063e386f485b1555d63341ff517273b
lipengzhou <lpzmail@163.com>
2020-04-08 01:35:20 +0800

    文章搜索-联想建议-优化防抖

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -12,6 +12,9 @@
 <script>
 import { getSearchSuggestions } from '@/api/search'
 
+// 按需加载有好处：只会把使用到的成员打包到结果中
+import { debounce } from 'lodash'
+
 export default {
   name: 'SearchSuggestion',
   components: {},
@@ -31,9 +34,20 @@ export default {
     searchText: {
       // 当 searchText 发生改变的时候就会调用 handler 函数
       // 注意：handler 函数名称是固定的
-      handler (value) {
+      // handler (value) {
+      //   console.log(value)
+      // },
+
+      // debounce 函数
+      // 参数1：一个函数
+      // 参数2：延迟时间，单位是毫秒
+      // 返回值：防抖之后的函数
+      handler: debounce(function (value) {
         this.loadSearchSuggestions(value)
-      },
+      }, 200),
+      // handler (value) {
+      //   this.loadSearchSuggestions(value)
+      // },
       immediate: true // 该回调将会在侦听开始之后被立即调用
     }
   },
