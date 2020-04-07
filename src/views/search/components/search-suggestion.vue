52229a0926e611fee6340615c3d5c6594a3866eb
lipengzhou <lpzmail@163.com>
2020-04-08 02:07:27 +0800

    文章搜索-联想建议-搜索关键字高亮

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -5,7 +5,7 @@
       v-for="(text, index) in suggestions"
       :key="index"
     >
-      <div slot="title" v-html="text"></div>
+      <div slot="title" v-html="highlight(text)"></div>
     </van-cell>
     <!-- 双花括号绑定会直接输出纯文本内容 -->
     <!-- <div>{{ htmlStr }}</div> -->
@@ -68,9 +68,27 @@ export default {
       } catch (err) {
         this.$toast('数据获取失败，请稍后重试')
       }
+    },
+
+    highlight (text) {
+      const highlightStr = `<span class="active">${this.searchText}</span>`
+
+      // 正则表达式 // 中间的内容都会当作匹配字符来使用，而不是数据变量
+      // 如果需要根据数据变量动态的创建正则表达式，则手动 new RegExp
+      // RegExp 正则表达式构造函数
+      //    参数1：匹配模式字符串，它会根据这个字符串创建正则对象
+      //    参数2：匹配模式，要写到字符串中
+      const reg = new RegExp(this.searchText, 'gi')
+      return text.replace(reg, highlightStr)
     }
   }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.search-suggestion {
+  /deep/ span.active {
+    color: #3296fa;
+  }
+}
+</style>
