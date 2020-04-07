0b297b928243343e769dafaf6d7380101439c39b
lipengzhou <lpzmail@163.com>
2020-04-07 23:19:53 +0800

    文章搜索-创建组件并配置路由

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,20 +1,5 @@
 <template>
-  <div class="search-container">
-    <!--
-      Tips: 在 van-search 外层增加 form 标签，且 action 不为空，即可在 iOS 输入法中显示搜索按钮
-     -->
-    <form action="/">
-      <van-search
-        class="search"
-        v-model="searchText"
-        show-action
-        placeholder="请输入搜索关键词"
-        background="#3296fa"
-        @search="onSearch"
-        @cancel="onCancel"
-      />
-    </form>
-  </div>
+  <div class="search-container">搜索页面</div>
 </template>
 
 <script>
@@ -23,32 +8,14 @@ export default {
   components: {},
   props: {},
   data () {
-    return {
-      searchText: ''
-    }
+    return {}
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {
-    onSearch (val) {
-      console.log(val)
-    },
-
-    onCancel () {
-      this.$router.back()
-    }
-  }
+  methods: {}
 }
 </script>
 
-<style scoped lang="less">
-.search-container {
-  .search {
-    .van-search__action {
-      color: #fff;
-    }
-  }
-}
-</style>
+<style scoped lang="less"></style>
