65be4850451e0ee417e9ffccdd2ccab5bb75b1d9
lipengzhou <lpzmail@163.com>
2020-04-08 20:28:17 +0800

    文章搜索-历史记录-处理删除显示状态

--- src/views/search/components/search-history.vue
+++ src/views/search/components/search-history.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="search-history">
     <van-cell title="搜索历史">
-      <span>全部删除</span>
-      <span>完成</span>
-      <van-icon name="delete" />
+      <div v-if="isDeleteShow">
+        <span>全部删除</span>
+        &nbsp;&nbsp;
+        <span @click="isDeleteShow = false">完成</span>
+      </div>
+      <van-icon v-else name="delete" @click="isDeleteShow = true" />
     </van-cell>
     <van-cell
       :title="item"
       v-for="(item, index) in searchHistories"
       :key="index"
     >
-      <van-icon name="close" />
+      <van-icon v-show="isDeleteShow" name="close" />
     </van-cell>
   </div>
 </template>
@@ -26,7 +29,9 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      isDeleteShow: false // 控制删除显示状态
+    }
   },
   computed: {},
   watch: {},
