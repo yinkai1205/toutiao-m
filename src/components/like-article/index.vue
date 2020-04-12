e90c2d7e2e27874724a023e08056d2e40d71b585
lipengzhou <lpzmail@163.com>
2020-04-12 15:13:18 +0800

    文章详情-文章点赞（处理完成）

--- src/components/like-article/index.vue
+++ src/components/like-article/index.vue
@@ -10,12 +10,18 @@
 </template>
 
 <script>
+import { addLike, deleteLike } from '@/api/article'
+
 export default {
   name: 'LikeArticle',
   components: {},
   props: {
     value: {
       type: Number,
+      required: true
+    },
+    articleId: {
+      type: [Number, String, Object],
       required: true
     }
   },
@@ -29,8 +35,27 @@ export default {
   created () {},
   mounted () {},
   methods: {
-    onCollect () {
-      console.log('onCollect')
+    async onCollect () {
+      this.loading = true
+      try {
+        let status = -1
+        if (this.value === 1) {
+          // 已点赞，取消点赞
+          await deleteLike(this.articleId)
+        } else {
+          // 没有点赞，添加点赞
+          await addLike(this.articleId)
+          status = 1
+        }
+
+        // 更新视图
+        this.$emit('input', status)
+        this.$toast.success(status === 1 ? '点赞成功' : '取消点赞')
+      } catch (err) {
+        console.log(err)
+        this.$toast.fail('操作失败，请重试！')
+      }
+      this.loading = false
     }
   }
 }
