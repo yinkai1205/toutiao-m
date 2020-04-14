50b51c2540e167a5ce1ffb017792fd36aa7a16e8
lipengzhou <lpzmail@163.com>
2020-04-14 15:12:28 +0800

    文章评论-评论回复-发布回复（处理底部视图布局）

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -9,17 +9,26 @@
         @click="$emit('close')"
       />
     </van-nav-bar>
-    <!-- 当前评论项 -->
-    <comment-item :comment="comment" />
-    <!-- /当前评论项 -->
 
-    <!-- 评论的回复列表 -->
-    <van-cell title="全部回复" />
-    <comment-list
-      :source="comment.com_id"
-      type="c"
-    />
-    <!-- /评论的回复列表 -->
+    <div class="scroll-wrap">
+      <!-- 当前评论项 -->
+      <comment-item :comment="comment" />
+      <!-- /当前评论项 -->
+
+      <!-- 评论的回复列表 -->
+      <van-cell title="全部回复" />
+      <comment-list
+        :source="comment.com_id"
+        type="c"
+      />
+      <!-- /评论的回复列表 -->
+    </div>
+
+    <!-- 发布评论 -->
+    <div class="reply-bottom">
+      <van-button class="write-btn" size="small" round>写评论</van-button>
+    </div>
+    <!-- /发布评论 -->
   </div>
 </template>
 
@@ -50,4 +59,29 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.scroll-wrap {
+  position: fixed;
+  top: 92px;
+  left: 0;
+  right: 0;
+  bottom: 88px;
+  overflow-y: auto;
+}
+
+.reply-bottom {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 88px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border-top: 1px solid #d8d8d8;
+  .write-btn {
+    width: 60%;
+  }
+}
+</style>
