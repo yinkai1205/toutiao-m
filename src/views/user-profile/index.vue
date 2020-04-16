b413c9523ebddf47a4e673880b0bb72a63216aa0
lipengzhou <lpzmail@163.com>
2020-04-17 00:19:06 +0800

    编辑用户资料-修改昵称-处理弹出层

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -18,7 +18,12 @@
         :src="user.photo"
       />
     </van-cell>
-    <van-cell title="昵称" :value="user.name" is-link />
+    <van-cell
+      title="昵称"
+      :value="user.name"
+      is-link
+      @click="isUpdateNameShow = true"
+    />
     <van-cell
       title="性别"
       :value="user.gender === 0 ? '男' : '女'"
@@ -26,6 +31,16 @@
     />
     <van-cell title="生日" :value="user.birthday" is-link />
     <!-- 个人信息 -->
+
+    <!-- 编辑昵称 -->
+    <van-popup
+      v-model="isUpdateNameShow"
+      style="height: 100%;"
+      position="bottom"
+    >
+      hello
+    </van-popup>
+    <!-- /编辑昵称 -->
   </div>
 </template>
 
@@ -38,7 +53,8 @@ export default {
   props: {},
   data () {
     return {
-      user: {} // 个人信息
+      user: {}, // 个人信息
+      isUpdateNameShow: false
     }
   },
   computed: {},
