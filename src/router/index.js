af24006a30cd74a5ed70259c1e826b18bef7b224
lipengzhou <lpzmail@163.com>
2020-04-16 23:38:55 +0800

    编辑用户资料-创建页面组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -47,6 +47,11 @@ const routes = [
     name: 'article',
     component: () => import('@/views/article'),
     props: true // 开启 Props 传参，说白了就是把路由参数映射到组件的 props 数据中
+  },
+  {
+    path: '/user/profile',
+    name: 'user-profile',
+    component: () => import('@/views/user-profile')
   }
 ]
 
