bdc4260ccd9394c136c6ad8a186e55edfc1c96e0
lipengzhou <lpzmail@163.com>
2020-04-17 00:03:38 +0800

    编辑用户资料-页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="user-profile">用户资料</div>
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="page-nav-bar"
+      title="个人信息"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+
+    <!-- 个人信息 -->
+    <van-cell title="头像" is-link>
+      <van-image
+        class="avatar"
+        fit="cover"
+        round
+        src="https://img.yzcdn.cn/vant/cat.jpeg"
+      />
+    </van-cell>
+    <van-cell title="昵称" value="内容" is-link />
+    <van-cell title="性别" value="内容" is-link />
+    <van-cell title="生日" value="内容" is-link />
+    <!-- 个人信息 -->
+  </div>
 </template>
 
 <script>
@@ -18,4 +41,11 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.user-profile {
+  .avatar {
+    width: 60px;
+    height: 60px;
+  }
+}
+</style>
