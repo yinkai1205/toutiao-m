358bf93a369c0995df5ad6686a2bf9cba6e5a97d
lipengzhou <lpzmail@163.com>
2020-04-19 20:07:31 +0800

    编辑用户资料-修改头像-图片上传预览（样式处理）

--- src/views/user-profile/components/update-photo.vue
+++ src/views/user-profile/components/update-photo.vue
@@ -1,5 +1,12 @@
 <template>
-  <img class="img" :src="img">
+  <div class="update-photo">
+    <img class="img" :src="img">
+
+    <div class="toolbar">
+      <div class="cancel" @click="$emit('close')">取消</div>
+      <div class="confirm">完成</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -24,6 +31,27 @@ export default {
 </script>
 
 <style scoped lang="less">
+.update-photo {
+  background-color: #000;
+  height: 100%;
+  .toolbar {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: space-between;
+    .cancel, .confirm {
+      width: 90px;
+      height: 90px;
+      font-size: 30px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #fff;
+    }
+  }
+}
 .img {
   max-width: 100%;
 }
